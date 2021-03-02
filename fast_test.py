b7c3057c1dd642b0994e406e22a0d4605f331793
RoroLopez <rorolopezag@gmail.com>
2021-03-02 13:58:01 -0600

    Pose recovery from Essential Matrix

--- fast_test.py
+++ fast_test.py
@@ -5,6 +5,7 @@ import pandas as pd
 import cv2 as cv
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
 from matplotlib import pyplot as plt
 
 cap = cv.VideoCapture('./video/testVideo.mp4')
@@ -22,8 +23,8 @@ last_frame = []
 R = np.zeros(shape=(3,3))
 t = np.zeros(shape=(3,1))
 
-F = 1
-#F = 272.93867730682706
+#F = 1
+F = 272.93867730682706
 
 K = np.array([[F,0,W//2], [0,F,H//2], [0,0,1]])
 Kinv = np.linalg.inv(K)
@@ -58,9 +59,9 @@ def normalize(inv, pts):
     return np.dot(inv, add_ones(pts).T).T[:,0:2]
 
 def denormalize(K, pts):
-    ret = np.dot(K, np.array([pts[0], pts[1], 1.0]))
-    ret /= ret[2]
-    return int(round(ret[0])), int(round(ret[1]))
+    result = np.dot(K, np.array([pts[0], pts[1], 1.0]))
+    result /= result[2]
+    return int(round(result[0])), int(round(result[1]))
 
 def add_ones(x):
     return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
@@ -71,9 +72,14 @@ def get_matches(Kinv,p1,p2):
 
     for m,n in matches:
         if m.distance < 0.75*n.distance:
-            pt1 = p1['kp1'][m.queryIdx].pt
-            pt2 = p2['kp2'][m.trainIdx].pt
+            pt1 = p1['kp'][m.queryIdx].pt
+            pt2 = p2['kp'][m.trainIdx].pt
             good.append((pt1,pt2))
+
+            # if np.linalg.norm(np.diff([pt1,pt2])) < 0.5*np.linalg.norm([W,H]) and m.distance < 32:
+            #     if m.queryIdx not in x1 and m.trainIdx not in x2:
+            #         x1.append(m.queryIdx)
+            #         x2.append(m.trainIdx)
         
     good = np.array(good)
 
@@ -81,8 +87,11 @@ def get_matches(Kinv,p1,p2):
     good[:,1,:] = normalize(Kinv,good[:,1,:])
 
     model,inliers = ransac((good[:,0], good[:,1]),
-                            FundamentalMatrixTransform,
-                            min_samples=8, residual_threshold=0.5,
+                            #FundamentalMatrixTransform,
+                            EssentialMatrixTransform,
+                            min_samples=8,
+                            #residual_threshold=0.5,
+                            residual_threshold=0.005,
                             max_trials=100)
 
     good = good[inliers]
@@ -90,6 +99,11 @@ def get_matches(Kinv,p1,p2):
     pose = extractRt(model.params)
 
     return good, pose
+
+frame1 = {}
+frame2 = {}
+
+global_pose = []
 
 while True:
     original_frame = cv.resize(frame, (W, H))
@@ -102,51 +116,36 @@ while True:
         ret, frame = cap.read()
         continue;
     else:
-        img = cv.drawKeypoints(resize_frame,kp2,None,color=(255,0,0), flags=None)
+        frame1['kp'] = kp1
+        frame1['des'] = des1
+        frame1['img'] = last_frame
 
-    matches = bf.knnMatch(des1, des2, k=2)
-    
-    good = []
-    x1,x2 = [],[]
+        frame2['kp'] = kp2
+        frame2['des'] = des2
+        frame2['img'] = resize_frame
 
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            pt1 = kp1[m.queryIdx].pt
-            pt2 = kp2[m.trainIdx].pt
-            good.append((pt1, pt2))
-        
-            # if np.linalg.norm(np.diff([pt1,pt2])) < 0.5*np.linalg.norm([W,H]) and m.distance < 32:
-            #     if m.queryIdx not in x1 and m.trainIdx not in x2:
-            #         x1.append(m.queryIdx)
-            #         x2.append(m.trainIdx)
+    good, pose = get_matches(Kinv, frame1, frame2)        
 
-    good = np.array(good)
-
-    # print(len(x1))
-    print(len(good))
-
-    good[:,0,:] = normalize(Kinv, good[:,0,:])
-    good[:,1,:] = normalize(Kinv, good[:,1,:])
-
-    model, inliers = ransac((good[:,0], good[:, 1]),
-                            FundamentalMatrixTransform,
-                            min_samples=8, residual_threshold=0.5,
-                            max_trials=100)
-
-    good = good[inliers]
-
-    pose = extractRt(model.params)
     # print(pose)
     # print(pose[:,3])
-    # maybe = denormalize(K,pose[:,3])
-    # print(maybe)
+    # print(pose[:,3] + pose)
+    maybe = denormalize(K,pose[:,3])
+    print(maybe)
+
+    # not sure if this is correct ... for Essential Matrix pose recovery
+    if len(global_pose) != 0:
+        global_pose = global_pose + pose[:,3]
+        # global_pose = global_pose[:,3]
+    else:
+        global_pose = pose[:,3]
+    
+    # print(global_pose)
+    # print(denormalize(K, global_pose))
 
     for pt1, pt2 in good:
         u1,v1 = denormalize(K,pt1)
         u2,v2 = denormalize(K,pt2)
 
-        # u1,v1 = map(lambda x: int(round(x)), pt1)
-        # u2,v2 = map(lambda x: int(round(x)), pt2)
         img = cv.circle(original_frame, (u1,v1), color=(255,255,0), radius=3)
         cv.line(original_frame, (u1,v1), (u2,v2), color=(255,0,0))
 
