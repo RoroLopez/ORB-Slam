2b3a0bc86496d59faf61c3e326c62691ef0035e1
RoroLopez <rorolopezag@gmail.com>
2021-03-07 00:16:55 -0700

    testing

--- matcher.py
+++ matcher.py
@@ -12,7 +12,7 @@ def get_features(frame):
     key_pts = [cv.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in pts]
     key_pts, descriptors = orb.compute(frame, key_pts)
 
-    return key_pts, descriptors
+    return np.array([(kp.pt[0], kp.pt[1]) for kp in key_pts]), descriptors
 
 def add_ones(x):
     return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
@@ -31,24 +31,32 @@ def extractRt(F):
     ret[:3, 3] = t
     return ret
 
-def get_pose(Kinv, frame1, frame2):
+def get_pose(frame1, frame2):
     bf = cv.BFMatcher(cv.NORM_HAMMING)
+
+    Kinv = frame1.Kinv
 
     matches = bf.knnMatch(frame1.des, frame2.des, k=2)
     good = []
+    x1,x2 = [],[]
 
     for m,n in matches:
         if m.distance < 0.75*n.distance:
-            pt1 = frame1.kp[m.queryIdx].pt
-            pt2 = frame2.kp[m.trainIdx].pt
+            x1.append(m.queryIdx)
+            x2.append(m.trainIdx)
+
+            pt1 = frame1.kp[m.queryIdx]
+            pt2 = frame2.kp[m.trainIdx]
             good.append((pt1,pt2))
         
     good = np.array(good)
+    x1 = np.array(x1)
+    x2 = np.array(x2)
 
     good[:,0,:] = normalize(Kinv,good[:,0,:])
     good[:,1,:] = normalize(Kinv,good[:,1,:])
 
-    model,inliers = ransac((good[:,0], good[:,1]),
+    model, inliers = ransac((good[:,0], good[:,1]),
                             #FundamentalMatrixTransform,
                             EssentialMatrixTransform,
                             min_samples=8,
@@ -57,10 +65,10 @@ def get_pose(Kinv, frame1, frame2):
                             max_trials=100)
 
     good = good[inliers]
-
     pose = extractRt(model.params)
 
-    return good, pose
+    # return good, pose
+    return x1[inliers], x2[inliers], pose, good
 
 
 def normalize(Kinv, pts):
@@ -71,9 +79,14 @@ def denormalize(K, pts):
     result /= result[2]
     return int(round(result[0])), int(round(result[1]))
 
-class Camera:
-    def __init__(self, K):
+# initial pose
+IRt = np.eye(4)
+
+class Frame(object):
+    def __init__(self, img, K):
         self.K = K
         self.Kinv = np.linalg.inv(K)
-        # self.R = np.zeros(shape=(3,3))
-        # self.t = np.zeros(shape=(3,1))
+        self.pose = IRt
+        
+        pts, self.des = get_features(img)
+        self.kp = normalize(self.Kinv, pts)
