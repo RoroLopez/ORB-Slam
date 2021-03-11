44a0363c12b3772d16982167a7db88125b0f1156
RoroLopez <rorolopezag@gmail.com>
2021-03-11 16:06:16 -0700

    Refactored IDs of frames

--- frame.py
+++ frame.py
@@ -3,7 +3,7 @@ from matcher import get_features, normalize
 
 IRt = np.eye(4)
 class Frame:
-    def __init__(self, img, K):
+    def __init__(self, img, mapp, K):
         self.K = K
         self.Kinv = np.linalg.inv(K)
         self.w, self.h = img.shape[:2]
@@ -12,3 +12,6 @@ class Frame:
 
         pts, self.des = get_features(img)
         self.kp = normalize(self.Kinv, pts)
+
+        self.id = len(mapp.frames)
+        mapp.frames.append(self)
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import cv2 as cv
 import OpenGL.GL as gl
 import pangolin
 
+from multiprocessing import Process, Queue
+import time
+import sys
+
 from matcher import Camera, get_pose, get_features, denormalize
 from frame import Frame, IRt
 
@@ -19,17 +23,71 @@ K = np.array([[F,0,W//2], [0,F,H//2], [0,0,1]])
 
 camera = Camera(K)
 
+# global map
 frames = []
+points = []
 
 def triangulatePoints(pose1, pose2, pts1, pts2):
     return cv.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts1.T).T
 
 
+q = Queue()
+# viewer = Process(target=generate_map, args=(self.q,))
+
+# gonna skip id identification of the frames and jump straight to the map generation in another thread..
+def generate_map():
+    pangolin.CreateWindowAndBind('Main', 640, 480)
+    gl.glEnable(gl.GL_DEPTH_TEST)
+
+    # Define Projection and initial ModelView matrix
+    scam = pangolin.OpenGlRenderState(
+        pangolin.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
+        pangolin.ModelViewLookAt(-2, 2, -2, 0, 0, 0, pangolin.AxisDirection.AxisY))
+    handler = pangolin.Handler3D(scam)
+
+    # Create Interactive View in window
+    dcam = pangolin.CreateDisplay()
+    dcam.SetBounds(0.0, 1.0, 0.0, 1.0, -640.0/480.0)
+    dcam.SetHandler(handler)
+
+    darr = None
+    while not pangolin.ShouldQuit():
+        darr = q.get(darr is None)
+
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+        dcam.Activate(scam)
+
+        gl.glPointSize(10)
+        gl.glColor3f(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(d[:3, 3] for d in darr[0])
+
+        gl.glPointSize(2)
+        gl.glColor3f(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(d for d in darr[1])
+
+        pangolin.FinishFrame()
+
+class Map(object):
+    def __init__(self):
+        self.frames = []
+        self.points = []
+
+    def display_map(self):
+        for f in self.frames:
+            print(f.id)
+            print(f.pose)
+    
+
+mapp = Map()
+
 class Point(object):
-    def __init__(self, loc):
+    def __init__(self, mapp, loc):
         self.location = loc
         self.frames = []
         self.idxs = []
+        self.id = len(mapp.points)
+        mapp.points.append(self)
     
     def add_observation(self, frame, idx):
         self.frames.append(frame)
@@ -39,21 +97,21 @@ while True:
     original_frame = cv.resize(frame, (W, H))
     resized_frame = cv.cvtColor(original_frame, cv.COLOR_BGR2GRAY)
 
-    frame = Frame(resized_frame, K)
-    # frame.kp, frame.des = get_features(frame.img)
+    frame = Frame(resized_frame, mapp, K)
     frames.append(frame)
 
-    if len(frames) <= 1:
+    if frame.id == 0:
         ret, frame = cap.read()
         continue;
+    
+    f1 = mapp.frames[-1]
+    f2 = mapp.frames[-2]
 
-    idx1, idx2, Rt = get_pose(camera.Kinv, frames[-1], frames[-2])
-    frames[-1].pose = np.dot(Rt, frames[-2].pose)
-    # print(len(idx1))
-    # print(frames[-1].pose)
+    idx1, idx2, Rt = get_pose(camera.Kinv, f1, f2)
+    f1.pose = np.dot(Rt, f2.pose)
 
     # homogenous 3D coordinates
-    pts3d = triangulatePoints(frames[-1].pose, frames[-1].pose, frames[-1].kp[idx1], frames[-2].kp[idx2])
+    pts3d = triangulatePoints(f1.pose, f2.pose, f1.kp[idx1], f2.kp[idx2])
     pts3d /= pts3d[:, 3:]
 
     # reject points without enough "parallax"...
@@ -61,22 +119,23 @@ while True:
     good_pts3d = (np.abs(pts3d[:,3]) > 0.005) & (pts3d[:, 2] > 0)
     pts3d = pts3d[good_pts3d]
     
-    print(pts3d)
+    # print(pts3d)
 
     for i,p in enumerate(pts3d):
         if not good_pts3d[i]:
             continue;
-        pt = Point(p)
-        pt.add_observation(frames[-1], idx1[i])
-        pt.add_observation(frames[-2], idx2[i])
+        pt = Point(mapp, p)
+        pt.add_observation(f1, idx1[i])
+        pt.add_observation(f2, idx2[i])
 
     for id1, id2 in zip(idx1, idx2):
-        u1,v1 = denormalize(K,frames[-1].kp[id1])
-        u2,v2 = denormalize(K,frames[-2].kp[id2])
+        u1,v1 = denormalize(K,f1.kp[id1])
+        u2,v2 = denormalize(K,f2.kp[id2])
         img = cv.circle(original_frame, (u1,v1), color=(255,255,0), radius=3)
         cv.line(original_frame, (u1,v1), (u2,v2), color=(255,0,0))
 
     cv.imshow('frame', img)
+    mapp.display_map()
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
